cb1884a4362458591ba2f99f0104e112ae1c8d79
Basma Hendy <basma.hendy886@gmail.com>
2021-01-28 07:14:56 -0800

    k

--- scripts.js
+++ scripts.js
@@ -9,11 +9,14 @@ $(document).ready(function(){
          div.stop(true,false).animate({width:"toggle"},850);
       
          
-        $("#home").stop(true,false).animate({fontSize:'45px'},400).animate({fontSize:'40px'},400);
-        $("#about").stop(true,false).animate({fontSize:'45px'},500).animate({fontSize:'40px'},300);
-        $("#contact").stop(true,false).animate({fontSize:'45px'},650).animate({fontSize:'40px'},250);
+        $("#home").stop(true,false).animate({fontSize:'42px'},400).animate({fontSize:'38px'},400);
+        $("#about").stop(true,false).animate({fontSize:'42px'},500).animate({fontSize:'38px'},300);
+        $("#contact").stop(true,false).animate({fontSize:'42px'},650).animate({fontSize:'38px'},250);
         
-        });
-            
+    });
+         
+    $("header").on("scroll",function(){
+        $("header").addClass("white");
+    });
       
     });
